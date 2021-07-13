8ad4f12995589f2dfa39c3e50f9816e4dd7a63ae
vara123prasad <varaprasadamursetti@gmail.com>
2021-07-12 22:54:06 -0400

    basics1

---
+++ basics.js
@@ -0,0 +1,12 @@
+//how to find a square root of a numbers
+var numbers =25; 
+var result = Math.sqrt(numbers);
+console.log(`The square root of ${numbers} is ${result}`);
+//program to check the number is divisible by 7 or not
+
+if(number1%7===0){
+	console.log( number1);
+	
+
+
+}
--- one.js
+++ one.js
@@ -1,1 +1,14 @@
-vara
+//program to print hello world
+console.log("hello world");
+//addind two numbers
+var a=1;
+var b=3;
+result=a+b;
+console.log(result);
+//to find square root
+// take the input from the user
+// take the input from the user
+const number1 = prompt("Enter the number");
+
+const result1 = Math.sqrt(number1);
+console.log(`The square root of ${number1} is ${result1}`);
