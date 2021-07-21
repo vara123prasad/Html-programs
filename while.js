ac9c787758a1b826daf809dc2ee093ea31ebea18
vara123prasad <varaprasadamursetti@gmail.com>
2021-07-21 02:21:46 -0400

    basics

---
+++ while.js
@@ -0,0 +1,56 @@
+//print 1 to 10 odd numbers
+for(i=1; i<=20; i=i+2 )
+{
+	console.log(i);
+}
+//4 table in while loop
+
+let j=4;
+while(j<=40){
+	
+	console.log(j);
+	j=j+4;
+}
+//6 table in while loop 
+let k=6;
+while(k<=60){
+	
+	console.log(k);
+	k=k+6;
+}
+//print reverse of a numbersd
+var numbers1 = [1,2,3,4,5,6,7,8,9,]
+    numbers1.reverse()
+	console.log(numbers1)
+//find the square root of a given numbers
+const squareroot=25;
+const result=Math.sqrt(squareroot);
+console.log(result);
+// area = (base * height) / 2    if we know base and height
+const basevalue=4;
+const heightvalue=6;
+const area=(basevalue*heightvalue)/2;
+console.log(area);
+//vsv
+let resut=1;
+for(i3=1; i3<=3; i3++){
+	resut=resut*i3;
+}
+  console.log(resut);
+  //hello world
+  console.log("hi varaprasad")
+  //print only the even number in the given numbers
+  
+  var a=[1,2,3,4,5,6,7,8,9]
+  for(p=0; p<= a.length; p++)
+  {
+	  
+	  if (p%2==0){
+		  console.log( p);
+	  
+	  }
+   }
+   //fibinossi number series to a givenj number
+
+// program to generate fibonacci series up to a certain number
+
